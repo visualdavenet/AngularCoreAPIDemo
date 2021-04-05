0aabb80f508a3cc7f2dd3484271ec5a8b27087d3
David Martinez <visualdave@live.com>
2021-04-05 13:04:47 -0400

    Updated API connection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,23 +10,23 @@ export class HomeComponent implements OnInit {
 
   constructor(private service:SharedService) { }
 
-  PeopleList:any=[];
-  PropertyList:any=[];
+  //PeopleList:any=[];
+  CustomerList:any=[];
   stringJson: any;
   stringObject: any;
 
   ngOnInit(): void {
-    this.refreshPropertyList();
+    this.refreshCustomerList();
     //this.refreshPeopleList();
   }
 
-  refreshPeopleList(){
-    this.service.getPeopleList().subscribe(data=>{ this.PeopleList=data; });
-    this.service.getPeopleList().subscribe(data => console.log(data));
-  }
+  // refreshPeopleList(){
+  //   this.service.getPeopleList().subscribe(data=>{ this.PeopleList=data; });
+  //   this.service.getPeopleList().subscribe(data => console.log(data));
+  // }
 
-  refreshPropertyList(){
-    this.service.getPropertyList().subscribe(data=>{ this.PropertyList=data; });
+  refreshCustomerList(){
+    this.service.getPropertyList().subscribe(data=>{ this.CustomerList=data; });
     this.service.getPropertyList().subscribe(res => console.log(res));
   }
 }
